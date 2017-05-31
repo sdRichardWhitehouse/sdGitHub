541ccc4d9eedf61e75de2f037a86071f6c54fa7d
sdRichardWhitehouse <richard.whitehouse@shopdirect.com>
2017-05-31 21:14:48 +0100

    Including All Sites

--- scriptChecker/scriptChecker.js
+++ scriptChecker/scriptChecker.js
@@ -84,9 +84,9 @@ module.exports = {
 					]
 				};
 				testList.push(veryUrls);
-				// testList.push(littlewoodsUrls);
-				// testList.push(littlewoodsIrelandUrls);
-				// testList.push(veryExclusiveUrls);
+				testList.push(littlewoodsUrls);
+				testList.push(littlewoodsIrelandUrls);
+				testList.push(veryExclusiveUrls);
 				
 				if (device == "Mobile") {
 					userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25";
